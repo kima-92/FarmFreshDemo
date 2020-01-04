bd08219d3d849e1fce71883e3d5101fa770682f6
Wilma <kima_92@fastmail.com>
2020-01-04 11:01:57 -0500

    Segues to Both Farmer's and Consumer's HomePage from LoginVC are Setup

--- View Controllers/LoginViewController.swift
+++ View Controllers/LoginViewController.swift
@@ -9,22 +9,45 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+        
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userTypeSegmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func LoginButtonTapped(_ sender: UIButton) {
+        
+        if userTypeSegmentedControl.selectedSegmentIndex == 0 {
+            performSegue(withIdentifier: "FarmerHomePageSegue", sender: self)
+            
+        } else if userTypeSegmentedControl.selectedSegmentIndex == 1 {
+            performSegue(withIdentifier: "ConsumerHomePageSegue", sender: self)
+        }
     }
-    */
-
+    
+    
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+//    // MARK: - Navigation
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        
+//        if segue.identifier == "FarmerHomePageSegue" {
+//            
+//            segue.destination as? FarmerHomeViewController
+//                      
+//        } else if segue.identifier == "ConsumerHomePageSegue" {
+//            
+//            segue.destination as? ConsumerHomeViewController
+//
+//        }
+//    }
 }
